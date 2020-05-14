9ec54863c584f8073ec81cd086ee8c18e96b74ed
Bryan Quadras <50770021+bqrs01@users.noreply.github.com>
2020-05-14 08:44:16 +0200

    Updated message.

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ const mainMessageEmbed = new Discord.MessageEmbed()
 	.setTitle('How to join voice?')
 	.setAuthor('QuadBot')
 	.addFields(
-		{ name: 'Instructions', value: 'First select the voice channel by reacting with the options below (1 or 2). Once you\'ve reacted, join the \'join-voice\' channel.'},
+		{ name: 'Instructions', value: 'First select the voice channel by reacting with the options below (1 or 2). Once you\'ve reacted, join the voice channel.'},
 		{ name: '\u200B', value: '\u200B' },
 		{ name: 'Main Project Discussion', value: 'React with 1.', inline: true },
 		{ name: 'CATIA Crew', value: 'React with 2.', inline: true },
