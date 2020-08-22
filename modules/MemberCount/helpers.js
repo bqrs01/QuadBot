81333f76c19361dc23588fe60f242aaa36c5ad41
Bryan Quadras <50770021+bqrs01@users.noreply.github.com>
2020-08-22 11:58:50 +0200

    Added guild fetch line in order to update member count correctly.

--- modules/MemberCount/helpers.js
+++ modules/MemberCount/helpers.js
@@ -1,4 +1,5 @@
 module.exports.updateMemberCount = async (channel, guild) => {
+    await guild.members.fetch() // Key line to ensure correct member count
     const members = await guild.members.cache.filter(a => (!a.user.bot))
     const memberCount = members.size
     try {
