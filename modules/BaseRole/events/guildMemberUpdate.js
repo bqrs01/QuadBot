cc3fff80be05928520ebe95841d95e14b2cf4f19
Bryan Quadras <50770021+bqrs01@users.noreply.github.com>
2021-02-19 18:43:40 +0100

    Fixed mistype.

--- modules/BaseRole/events/guildMemberUpdate.js
+++ modules/BaseRole/events/guildMemberUpdate.js
@@ -7,7 +7,7 @@ module.exports = async(client, oldMember, newMember) => {
     if (oldMember.pending && !newMember.pending) {
         // Get guildId to check if guild is setup
         guildId = oldMember.guild.id
-        if (moduleData.has('setups', guildId) && moduleData.get('setups', `${guildId}.enabled`) {
+        if (moduleData.has('setups', guildId) && moduleData.get('setups', `${guildId}.enabled`)) {
             // Guild is setup. Give base role to member.
             roleId = moduleData.get('setups', `${guildId}.roleId`)
             newMember.guild.roles.fetch(roleId).then(role => {
